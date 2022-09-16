851387c533c0e9f4e8c3509e9d589748b04fea7d
Ahiabuike Emmanuel <ahiabuikeuba@gmail.com>
2022-09-16 15:52:13 -0700

    emmy

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal
+ *
+ * @n: number of times the character \ should be printed
+ */
+void print_diagonal(int n)
+{
+int c;
+int i;
+c = 0;
+while (i > 0)
+{
+_putchar(' ');
+i--;
+}
+_putchar('\\');
+_putchar('\n');
+c++;
+n--;
+}
+if (c < 1)
+_putchar('\n');
+}
