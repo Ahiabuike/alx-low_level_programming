ec6dfeb0477a016d2b0599f9097c32ad49c1ad66
Ahiabuike Emmanuel <ahiabuikeuba@gmail.com>
2022-09-22 00:40:59 -0700

    uba

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination of concatenates
+ * @src: source array of concatenates
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
