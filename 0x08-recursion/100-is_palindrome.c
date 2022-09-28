60be6e5b0cefc34260be8aa5ea6c659f5a10bd70
Ahiabuike Emmanuel <ahiabuikeuba@gmail.com>
2022-09-28 13:56:13 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,6 +9,10 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
+if (*s == 0)
+return (1);
+return (check_pal(s, 0, _strlen_recursion(s)));
+}
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
@@ -34,5 +38,6 @@ int check_pal(char *s, int i, int len)
 if (*(s + i) != *(s + len - 1))
 return (0);
 if (i >= len)
+return (1);	
 return (check_pal(s, i + 1, len - 1));
 }
