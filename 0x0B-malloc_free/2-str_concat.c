2007646b9af1de22618a788051031a806825d281
ahiabuike <ahiabuikeuba@gmail.com>
2022-10-04 18:35:48 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *s3;
 unsigned int i = 0, j = 0, len1 = 0, len3 = 0;
 while (s1 && s1[len1])
 len1++;
-while (s2 && s2[len2])
-len2++;
-s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+while (s2 && s2[len3])
+len3++;
+s3 = malloc(sizeof(char) * (len1 + len3 + 1));
 if (s3 == NULL)
 return (NULL);
 i = 0;
@@ -30,7 +30,7 @@ i++;
 }
 if (s2)
 {
-while (i < (len1 + len2))
+while (i < (len1 + len3))
 {
 s3[i] = s2[j];
 i++;
